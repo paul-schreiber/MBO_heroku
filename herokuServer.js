478accb4a8dd834abd9ea72a7ed13e92b7e8c9e9
Paul Schreiber <schreiber.paul96@gmail.com>
2019-05-28 19:40:29 +0200

    add restart function

--- herokuServer.js
+++ herokuServer.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const app = express();
-let lastValue;
+const express = require('express')
+const app = express()
+let lastValue
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function (req, res) {
-  if(req.query.command === ""){
-    res.send("{ \"command\":\"" + lastValue + "\"}");
-  }else{
-    if(req.query.command === "empty"){
-      lastValue = "";
-      res.send("{}");
-    }else{
-      res.send("{ \"command\":\"" + req.query.command + "\"}");
-      lastValue = req.query.command;
+  if (req.query.command === '') {
+    res.send('{ "command":"' + lastValue + '"}')
+  } else {
+    if (req.query.command === 'empty') {
+      lastValue = ''
+      res.send('{}')
+    } else if (req.query.command === 'restart-restart') {
+      lastValue = ''
+      res.send('{restart}')
+    } else {
+      res.send('{ "command":"' + req.query.command + '"}')
+      lastValue = req.query.command
     }
   }
 })
 
 app.listen(app.get('port'), function () {
-  console.log("Node app is running on port', app.get('port')");
+  console.log('Node app is running on port\', app.get(\'port\')')
 })
