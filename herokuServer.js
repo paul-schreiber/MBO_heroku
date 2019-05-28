481f350fecbfdecd3375e224b0ad461b10e60b82
Paul Schreiber <schreiber.paul96@gmail.com>
2019-05-28 17:54:08 +0200

    add express server

--- herokuServer.js
+++ herokuServer.js
@@ -1,22 +1,23 @@
 const express = require('express');
-const path = require('path');
-const socketIO = require('socket.io');
+const app = express();
+let lastValue;
 
+app.set('port', (process.env.PORT || 5000));
 
-let PORT = 3000;
-const INDEX = path.join(__dirname, 'console.html');
+app.get('/', function (req, res) {
+  if(req.query.command === ""){
+    res.send("{ \"command\":\"" + lastValue + "\"}");
+  }else{
+    if(req.query.command === "empty"){
+      lastValue = "";
+      res.send("{}");
+    }else{
+      res.send("{ \"command\":\"" + req.query.command + "\"}");
+      lastValue = req.query.command;
+    }
+  }
+})
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`));
-
-const io = socketIO(server);
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-  socket.on("shoot", (payload) => {
-    console.log(payload);
-    socket.broadcast.emit("shoot", payload);
-  });
-
-});
+app.listen(app.get('port'), function () {
+  console.log("Node app is running on port', app.get('port')");
+})
