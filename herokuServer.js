07e408820b89bb9803fcfd34203df72fa1a79a0c
Paul Schreiber <schreiber.paul96@gmail.com>
2019-05-28 15:27:38 +0200

    add dynamic log

--- herokuServer.js
+++ herokuServer.js
@@ -8,10 +8,11 @@ const INDEX = path.join(__dirname, 'console.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
-  .listen(process.env.PORT || PORT, () => console.log(`Listening on ${ PORT }`));
+  .listen(process.env.PORT || PORT, () => console.log(`Listening on ${process.env.PORT} or ${ PORT }`));
 
 const io = socketIO(server);
 io.on('connection', (socket) => {
+  socket.emit("received", "test");
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
   socket.on("shoot", (payload) => {
